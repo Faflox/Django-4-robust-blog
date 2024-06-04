5a4f6ec1f8bd77b65ad06ba219c06dd01c22a1de
Rafał Tomczyk <rafal.tomczyk2000@gmail.com>
2024-06-04 16:58:00 +0200

    added comment, tag functionality

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
+from taggit.managers import TaggableManager
 
 # Create your models here.
 class PublishedManager(models.Manager):
@@ -25,6 +26,7 @@ class Post(models.Model):
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
     object = models.Manager()
     published = PublishedManager()
+    tags = TaggableManager()
     
     class Meta:
         ordering = ['-publish']
@@ -41,3 +43,23 @@ class Post(models.Model):
                                                  self.publish.day,
                                                  self.slug])
     
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+        
+    def __str__(self):
+        return f'Komentarz dodany przez {self.name} dla posta {self.post}'
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,14 +1,20 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
+from django.views.decorators.http import require_GET, require_POST
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.http import Http404
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
+from taggit.models import Tag
 
 # Create your views here.
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag= None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
     try:
@@ -19,7 +25,8 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'posts': posts,
+                   'tag': tag})
     
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -34,11 +41,17 @@ def post_detail(request, year, month, day, post):
                             publish__year = year,
                             publish__month = month,
                             publish__day = day)
+    #komentarze
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
     
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
-    
+                  {'post': post,
+                   'comments': comments,
+                   'form': form})
+
+
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
     sent = False
@@ -57,3 +70,20 @@ def post_share(request, post_id):
     return render(request, 'blog/post/share.html', {'post': post,
                                                     'form': form,
                                                     'sent': sent})
+    
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    #Opublikowano komentarz
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        #commit=False nie zapisuje komentarza w bazie danych
+        #musimy tak zrobic, zeby przypisac odpowiedni post do komentarza po otrzymaniu formularza
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html',
+                  {'post': post,
+                   'form': form,
+                   'comment': comment})
